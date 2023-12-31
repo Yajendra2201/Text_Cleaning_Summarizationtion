1c18397c83d4ee4a8cdbaac404f079c838d92b2b
Yajendra_Prajapati <86663172+Yajendra2201@users.noreply.github.com>
2023-12-31 19:59:51 +0530

    Update text_cleaning_summarzation_app.py

--- text_cleaning_summarzation_app.py
+++ text_cleaning_summarzation_app.py
@@ -125,12 +125,13 @@ def extract_text_from_url(url):
 def local_css(file_name):
     with open(file_name, "r") as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
+        
+# Set page configuration
+st.set_page_config(layout="wide", page_title="Advanced Text Cleaning App")
 # Apply the custom CSS
 local_css("style.css")
 
-# Set page configuration
-st.set_page_config(layout="wide", page_title="Advanced Text Cleaning App")
+
 
 # Streamlit interface setup
 st.title('Advanced Text Cleaning App')
